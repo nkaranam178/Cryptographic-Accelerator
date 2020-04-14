e16889a4dcc5f914cbb403da1cf4046b45046e67
Phughes13 <56485653+Phughes13@users.noreply.github.com>
2020-04-14 16:15:19 -0500

    Add files via upload

--- cpu_module/If.v
+++ cpu_module/If.v
@@ -1,29 +1,47 @@
-module If(clk, rst_n, branch, target_pc, prev_pc, instr_to_write, ImemWrite, cur_pc, instr);
+module If(clk, rst_n, branch, target_pc, instr_to_write, ImemWrite, cur_pc, instr, ImemStall, DmemStall);
 
-input clk, rst_n, branch, ImemWrite;
-input[15:0] target_pc, prev_pc, instr_to_write;
+input clk, rst_n, branch, ImemWrite, DmemStall;
+input[15:0] target_pc, instr_to_write;
 output[15:0] instr;
+reg stall, stall2;
+reg[15:0] addr;
 output reg[15:0] cur_pc;
+output ImemStall;
 
+assign ImemStall = (stall | stall2);
 
 // Pc mux
 always @(posedge clk, negedge rst_n) begin
-	if (!rst_n) 
+	if (!rst_n) begin
+	  stall <= 1;
 	  cur_pc <= 0;
-	else if (branch)
+	  addr <= 0;
+	end
+	else if (stall | stall2) begin
+	  stall <= 0;
+	  cur_pc <= cur_pc;
+	  addr <= addr + 2;
+	end
+	else if (branch) begin
+	  stall <= 1;
 	  cur_pc <= target_pc;
-	else
-	  cur_pc <= prev_pc + 2;
+	  addr <= target_pc;
+	end
+	else begin
+	  stall <= 0;
+	  cur_pc <= cur_pc + 2;
+	  addr <= addr + 2;
+	end
 end
 
 
 
-// TAKE OUT
-assign instr = 16'h0;
+always @(posedge clk)
+	stall2 <= stall;
 
 
 // BRAM IP
-BRAM Imem(.address(cur_pc[8:0]), .clock(clk), .data(instr_to_write), .wren(ImemWrite), .q(instr));
+BRAM Imem(.address(addr[8:0]), .clock(clk), .data(instr_to_write), .wren(ImemWrite), .q(instr));
 
 
 endmodule
